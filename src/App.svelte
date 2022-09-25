8dee69c7e986fa7af68bffd4a5e0926468e8e839
Umar Mohammad <60586376+avidcoder123@users.noreply.github.com>
2022-09-25 13:14:28 -0500

    Move routing data to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
   import { Router } from 'svelte-router-spa'
-  import { routes } from './routes'
+  //import { routes } from './routes'
+  import Landing from "src/pages/Landing.svelte"
+
+  let routes = [
+    {
+      name: "/",
+      component: Landing
+    }
+  ]
 </script>
 
 <Router {routes} />
