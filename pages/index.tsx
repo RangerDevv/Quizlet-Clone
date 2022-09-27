7b5ee933cd6d4c6d6b61287eb73edb80b4342f3c
Rangerdevv <meerabdul2008@gmail.com>
2022-09-27 17:48:03 -0400

    added tailwind and DaisyUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import React from 'react'
+import Checkbox from 'react-daisyui/dist/Checkbox'
 
 const Home: NextPage = () => {
   return (
     <div>
     <h1>Hello, world! Welcome to Doki.</h1>
-    <p className='Hello'>This is a test!</p>
+    <p className=' text-blue-500'>This is a test!</p>
+    <button className='btn btn-error'>hello world</button>
     </div>
   )
 }
