abebd9f473afda1ceb9134457ddb45ffb4513366
Rangerdevv <meerabdul2008@gmail.com>
2022-09-27 15:56:13 -0400

    just testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
+    <div>
     <h1>Hello, world! Welcome to Doki.</h1>
+    <p className='Hello'>This is a test!</p>
+    </div>
   )
 }
 
