b79b40ad766c450668f6ee2fb38ac475b0f7d86f
Rangerdevv <meerabdul2008@gmail.com>
2022-09-28 21:39:27 -0400

    a fresh start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
-import Checkbox from 'react-daisyui/dist/Checkbox'
-
+import Flashcards from '../public/Flashcards.png'
 const Home: NextPage = () => {
   return (
-    <div>
-    <h1>Hello, world! Welcome to Doki.</h1>
-    <p className=' text-blue-500'>This is a test!</p>
-    <button className='btn btn-error'>hello world</button>
-    </div>
+    <h1>Yo</h1>
   )
 }
 
