1036903006f41486e4fa1145c7938074423b862b
avidcoder123 <umar.tli.edu@gmail.com>
2022-10-02 23:56:46 +0000

    Fix hydration bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Head from 'next/head'
 import React from 'react'
 const Home: NextPage = () => {
   return (
-    <body>
-      <nav className=' flex w-screen flex-row h-12 bg-slate-800'>
+    <div>
+      <nav className="flex w-screen flex-row h-12 bg-slate-800">
       </nav>
-    </body>
+    </div>
   )
 }
 
