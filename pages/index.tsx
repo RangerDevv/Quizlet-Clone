7a27c00f8bba52abf9f93885b10e4e8eb004d1ce
MEER2008 <meerabdul2008@gmail.com>
2022-10-03 12:57:06 +0000

    added some smol changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,11 @@ const Home: NextPage = () => {
         <p className=' text-5xl text-white text-center mt-8 w-96 leading-tight '>Work <u className=' underline decoration-4 decoration-sky-500 underline-offset-4'>Smarter</u>, Not Harder</p>
         </div>
         <p className=' text-xl text-center text-white'>With tau, you can study faster, and smarter.</p>
+        <div className=' flex justify-center'>
+        <button className=' btn w-36 btn-success'>Get Started →</button>
+        </div>
       </header>
     </div>
   )
 }
-
 export default Home
