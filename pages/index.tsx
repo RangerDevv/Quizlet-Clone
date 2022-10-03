f5ab23cfb148062b51555ac35b726b1c3df7c1ac
Rangerdevv <meerabdul2008@gmail.com>
2022-10-02 20:08:37 -0400

    stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 import React from 'react'
 const Home: NextPage = () => {
   return (
-    <div className='Body'>
-      <nav className=' flex w-screen flex-row h-12 bg-slate-800'>
+    <div className='Body bg-gray-900'>
+      <nav className=' flex w-screen flex-row h-12 bg-gray-700'>
         <p className=' text-3xl text-white ml-5 mt-1'>Tau</p>
         <div className='flex flex-row justify-end mr-5 w-screen'>
         <p className=' text-2xl text-white ml-5 mt-2 hover:mt-1'>Explore</p>
