1a76b59afda267f4afbc2f70ac819a28b52765a2
Rangerdevv <meerabdul2008@gmail.com>
2022-10-02 20:02:19 -0400

    finish navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,16 @@ import Head from 'next/head'
 import React from 'react'
 const Home: NextPage = () => {
   return (
-    <body>
+    <div className='Body'>
       <nav className=' flex w-screen flex-row h-12 bg-slate-800'>
+        <p className=' text-3xl text-white ml-5 mt-1'>Tau</p>
+        <div className='flex flex-row justify-end mr-5 w-screen'>
+        <p className=' text-2xl text-white ml-5 mt-2 hover:mt-1'>Explore</p>
+        <p className=' text-2xl text-white ml-5 mt-2 hover:mt-1'>Login</p>
+        <p className=' text-2xl text-white ml-5 mt-2 hover:mt-1'>Sign Up</p>
+        </div>
       </nav>
-    </body>
+    </div>
   )
 }
 
